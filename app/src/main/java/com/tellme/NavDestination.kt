7248b19e1ba8578831244b21f0f72783410a5876
Kirill Ivanov <ivanov.ks101@gmail.com>
2025-01-05 02:19:24 +0500

    created a rough nav graph

--- app/src/main/java/com/tellme/NavDestination.kt
+++ app/src/main/java/com/tellme/NavDestination.kt
@@ -4,5 +4,17 @@ import kotlinx.serialization.Serializable
 
 sealed class NavDestination {
     @Serializable
+    data object Onboarding : NavDestination()
+
+    @Serializable
     data object MainScreen : NavDestination()
+
+    @Serializable
+    data object NewNoteScreen : NavDestination()
+
+    @Serializable
+    data class NoteDetailsScreen(val noteId: Int) : NavDestination()
+
+    @Serializable
+    data class NotesForDayScreen(val day: Int) : NavDestination()
 }
