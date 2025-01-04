f3a74b385b4a022384bcc7e49f6b9bc8f1f971c1
Kirill Ivanov <ivanov.ks101@gmail.com>
2025-01-05 01:53:24 +0500

    added some base view model functionality

--- core-ui/src/main/java/com/tellme/core_ui/utils/BaseViewModel.kt
+++ core-ui/src/main/java/com/tellme/core_ui/utils/BaseViewModel.kt
@@ -5,11 +5,19 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-open class BaseViewModel<T>(
+open class BaseViewModel<T, I>(
     initState: T
 ) : ViewModel() {
 
     private val _state = MutableStateFlow(initState)
     val state: StateFlow<T>
         get() = _state.asStateFlow()
+
+    private val _currentIntent = MutableStateFlow<I?>(null)
+    val currentIntent: StateFlow<I?>
+        get() = _currentIntent.asStateFlow()
+
+    fun setIntent(newIntent: I) {
+        _currentIntent.value = newIntent
+    }
 }
