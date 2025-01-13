9e1ecc221c8feef1f9fb17ee46a22adf0c12d25c
Kirill <ivanov.ks101@gmail.com>
2025-01-13 13:07:34 +0500

    added some resources

---
+++ core-ui/src/main/java/com/tellme/core_ui/component/BottomNavigationBarSurface.kt
@@ -0,0 +1,9 @@
+package com.tellme.core_ui.component
+
+import androidx.compose.ui.graphics.Brush
+import com.tellme.core_ui.theme.Green
+import com.tellme.core_ui.theme.GreenLight
+
+val bottomNavBarGradient = Brush.verticalGradient(
+    listOf(GreenLight, Green)
+)
